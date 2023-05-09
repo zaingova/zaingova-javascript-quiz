4613f2d8f42149d7c8589a5ac4f34304722c8123
zaingova <zain.gova.school@gmail.com>
2023-05-09 11:35:35 -0400

    added better functionality to the JS logic. Added a function to compare the user input to the correct answer for that particular question

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,35 +20,39 @@ var timeRemaining;
 var finishedQuiz;
 var score;
 
+// global var that helps iterate through question numbers
+var questionNum = 0;
+
 // array of objects, containing the question, the possible answers, and the correct answer
 var answers = [
     {
-        "question": "#1. What does CSS stand for?",
+        "question": "What does CSS stand for?",
         "options": ["Cascading Style Sheets", "Computational Software System", "Call Set Style", "Correspondance Solutions Systems"],
         "answer": "Cascading Style Sheets"
     },
     {
-        "question": "#2. What does CSS allow us to do?",
+        "question": "What does CSS allow us to do?",
         "options": ["Style the HTML elements", "Add interactivity to the webpage", "Use various 3rd-party APIs", "Sort items in a list"],
         "answer": "Style the HTML elements"
     },
     {
-        "question": "#3. What does API stand for?",
+        "question": "What does API stand for?",
         "options": ["Application Programming Interface", "Artificial Part Interaction", "Analogus Prefrontal Interface", "Anti-Propogation ID"],
         "answer": "Application Programming Interface"
     },
     {
-        "question": "#4. What is the DOM?",
+        "question": "What is the DOM?",
         "options": ["A CSS styling guide", "Javascript tool used for iteration", "Document Object Model", "HTML syntax guidelines"],
         "answer": "Document Object Model"
     },
     {
-        "question": "#5. Which part of the HTML document does the JavaScript tag go into?",
+        "question": "Which part of the HTML document does the JavaScript tag go into?",
         "options": ["Top of the head", "Top of the body", "It doesn't go in the HTML document", "Bottom of the body"],
         "answer": "Bottom of the body"
     }
 ]
 
+// players initial and score get stored here
 var playerData = [];
 
 // function that displays the introduction screen
@@ -65,44 +69,81 @@ function displayIntro() {
 // renders the quiz section of the program
 function displayQuiz() {
     startTimer();
-    displayQuestion(0);
-    displayQuestion(1);
-    displayQuestion(2);
-    displayQuestion(3);
-    displayQuestion(4);
+
+    // starts at index 0, and progressively cycles through each question
+    displayQuestion(questionNum);
+    questionNum++;
+    displayQuestion(questionNum);
+    questionNum++;
+    displayQuestion(questionNum);
+    questionNum++;
+    displayQuestion(questionNum);
+    questionNum++;
+    displayQuestion(questionNum);
 }
 
 // renders each indivual set of questions and answers
-function displayQuestion(qindex) {
+function displayQuestion(questionNum) {
     // sets the question value
-    quizQuestion.textContent = answers[qindex].question;
+    quizQuestion.textContent = "#" + (questionNum+1) + ". " + answers[questionNum].question;
 
     // sets the
-    a1.textContent = "a) " + answers[qindex].options[0];
+    a1.textContent = "a) " + answers[questionNum].options[0];
     quizAnswers.appendChild(a1);
-    a2.textContent = "b) " + answers[qindex].options[1];
+    a2.textContent = "b) " + answers[questionNum].options[1];
     quizAnswers.appendChild(a2);
-    a3.textContent = "c) " + answers[qindex].options[2];
+    a3.textContent = "c) " + answers[questionNum].options[2];
     quizAnswers.appendChild(a3);
-    a4.textContent = "d) " + answers[qindex].options[3];
+    a4.textContent = "d) " + answers[questionNum].options[3];
     quizAnswers.appendChild(a4);
 }
 
 // event listener to clear the header content whem the 'start' button is clicked
-startButton.addEventListener("click", function () {
+startButton.addEventListener("click", function (){
     introEl.textContent = " ";
     quizQuestion.setAttribute("style", "display: block");
     init();
 })
+
+// event listeners for each possible answer -- looks for a 'click' event
+a1.addEventListener("click", function(event) {
+    parseInput(0);
+});
+
+a2.addEventListener("click", function(event) {
+    parseInput(1);
+});
+
+a3.addEventListener("click", function(event) {
+    parseInput(2);
+});
+
+a4.addEventListener("click", function(event) {
+    parseInput(3);
+});
+
+// compares the user input to the CORRECT answer in the question object
+function parseInput(index) {
+    // saves a new variable with the content of the answer minus the 'a) '
+    var stringAnswer = JSON.stringify(event.target.textContent);
+    stringAnswer = (stringAnswer.substring(4, stringAnswer.length - 1));
+
+    console.log(stringAnswer === answers[questionNum].answer);
+       
+    index++;
+}
 
 // initializer function. sets game state, starts timer, etc
 function init() {
     score = 0;
     timeRemaining = 60;
     finishedQuiz = false;
+    currentQ = 0;
+    questionIndex = 0;
     displayQuiz();
 }
 
+// displays highscores to the screen
 function renderHighscored() {
 
 }
@@ -142,4 +183,5 @@ function startTimer() {
     }, 1000);
 }
 
+// starts the quiz with the intro screen. Didnt use the init function because that would start the timer
 displayIntro();
